71964396441013f3d401b875f579cc6a01d59185
GamingGuy003 <gamingguy003@mail.com>
2023-08-23 11:05:10 +0200

    setup baseline for handling api requests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::SocketAddr;
+
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(test));
+    let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn test() -> String {
+    println!("Handled get to /");
+    String::new()
 }
