e91c9c25f2fe8223497a333333d8774efb090d80
GamingGuy003 <bjornfelixh@gmail.com>
2023-08-31 23:59:27 +0000

    added sqlite

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,58 @@
 use std::{net::SocketAddr, sync::{Arc, Mutex}};
 
 use axum::{extract::ConnectInfo, Router};
-use logger::{logger::Logger, level::Level::Info, log_field::LogField::{Date, Time, Seperator, Message, Level}};
-
-
-static LOGGER: Arc<Mutex<Logger>> = Arc::new(Mutex::new(Logger::new(Info, std::io::stdout(), vec![Date, Seperator(String::from("T")), Time, Seperator(String::from(" ")), Level, Seperator(String::from("")), Message])));
+use sqlite::Connection;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> std::io::Result<()> {
+    // create server addr
     let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
+
+    // connect to sqlite
+    let sql_connection = Arc::new(Mutex::new(match sqlite::open("./db") {
+        Ok(connection) => connection,
+        Err(err) => return Err(std::io::Error::new(std::io::ErrorKind::NotConnected, err))
+    }));
+
+    // define routes
     let app = Router::new()
         .route("/:uri", axum::routing::get(follow))
         .route("/_shorten/:uri", axum::routing::post(shorten))
         .route("/_delete/:uri", axum::routing::delete(delete))
         .route("/_info/:uri", axum::routing::get(info));
+
+    // startup webserver
     axum::Server::bind(&addr)
         .serve(app.into_make_service_with_connect_info::<SocketAddr>())
         .await
         .unwrap();
+
+    Ok(())
 }
 
-async fn follow(ConnectInfo(connection): ConnectInfo<SocketAddr>) {}
-async fn shorten(ConnectInfo(connection): ConnectInfo<SocketAddr>) {}
-async fn delete(ConnectInfo(connection): ConnectInfo<SocketAddr>) {}
-async fn info(ConnectInfo(connection): ConnectInfo<SocketAddr>) {}
+async fn initialize_db(mut connection: Connection) -> std::io::Result<()> {
+    let query = "
+        CREATE TABLE links(
+            original LONGTEXT,
+            shortened TEXT,
+        );
+    ";
+    connection.execute(query).unwrap();
+    Ok(())
+}
+
+async fn follow(ConnectInfo(connection): ConnectInfo<SocketAddr>) {
+
+}
+
+async fn shorten(ConnectInfo(connection): ConnectInfo<SocketAddr>) {
+
+}
+
+async fn delete(ConnectInfo(connection): ConnectInfo<SocketAddr>) {
+
+}
+
+async fn info(ConnectInfo(connection): ConnectInfo<SocketAddr>) {
+
+}
